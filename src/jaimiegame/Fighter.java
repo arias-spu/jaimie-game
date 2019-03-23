a427a523a03a12cfdd29d847d2f678fac28cccdc
Carlos R. Arias <arias@spu.edu>
2019-03-22 18:44:29 -0700

    Game working

--- src/jaimiegame/Fighter.java
+++ src/jaimiegame/Fighter.java
@@ -10,6 +10,11 @@ package jaimiegame;
  * @author arias
  */
 public class Fighter {
+    
+    public static final int SHIELD_ON = 1;
+    public static final int LIFE_ON = 2;
+    public static final int DEAD = 3;
+    
     private int shield;
     private int attack;
     private int life;
@@ -24,8 +29,52 @@ public class Fighter {
 
     @Override
     public String toString() {
-        return "Fighter{" + "shield=" + shield + ", attack=" + attack + ", life=" + life + '}';
+        return "Fighter{" + "name=" + name + ", shield=" + shield + ", attack=" + attack + ", life=" + life + '}';
     }
-    
+
+    public int getShield() {
+        return shield;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int attack(int power){
+        if (power > life){
+            return 0;
+        }
+        if (power > 10 || power < 0){
+            return -1;
+        }
+        life -= power;
+        return (int)Math.round(power / 10.0 * attack * Math.random());
+    }
+    public int defend(int attack){
+        if (shield > attack){
+            shield -= attack;
+            return SHIELD_ON;
+        }else{
+            if (shield > 0){
+                attack -= shield;
+                shield = 0;
+            }
+            if (life > attack){
+                life -= attack;
+                return LIFE_ON;
+            }else{
+                life = 0;
+                return DEAD;
+            }
+            
+        }
+    }
     
 }
--- src/jaimiegame/JaimieGame.java
+++ src/jaimiegame/JaimieGame.java
@@ -12,8 +12,9 @@ import java.util.Scanner;
  * @author arias
  */
 public class JaimieGame {
+
     private static Scanner input;
- 
+
     private static final int PALADIN_ATTACK = 70;
     private static final int PALADIN_LIFE = 100;
     private static final int PALADIN_SHIELD = 100;
@@ -21,7 +22,7 @@ public class JaimieGame {
     private static final int FIGHTER_ATTACK = 20;
     private static final int FIGHTER_LIFE = 100;
     private static final int FIGHTER_SHIELD = 70;
-    
+
     private static final int RANGER_ATTACK = 20;
     private static final int RANGER_LIFE = 100;
     private static final int RANGER_SHIELD = 70;
@@ -34,19 +35,19 @@ public class JaimieGame {
         String name;
         char type;
         Fighter player1 = null, player2 = null;
-        
+
         System.out.println("Welcome to the Battle!\n");
         System.out.println("Player 1 = = = = = = = = = =");
         System.out.print("What is your name: ");
         name = input.nextLine();
         type = selectType();
-        if (type == 'a'){
+        if (type == 'a') {
             player1 = new Fighter(name, PALADIN_SHIELD, PALADIN_ATTACK, PALADIN_LIFE);
-        }else if (type == 'b'){
+        } else if (type == 'b') {
             player1 = new Fighter(name, FIGHTER_SHIELD, FIGHTER_ATTACK, FIGHTER_LIFE);
-        }else if (type == 'c'){
+        } else if (type == 'c') {
             player1 = new Fighter(name, RANGER_SHIELD, RANGER_ATTACK, RANGER_LIFE);
-        }else{
+        } else {
             System.err.println("This should never happen!");
             System.exit(1);
         }
@@ -55,37 +56,89 @@ public class JaimieGame {
         System.out.print("What is your name: ");
         name = input.nextLine();
         type = selectType();
-        if (type == 'a'){
+        if (type == 'a') {
             player2 = new Fighter(name, PALADIN_SHIELD, PALADIN_ATTACK, PALADIN_LIFE);
-        }else if (type == 'b'){
+        } else if (type == 'b') {
             player2 = new Fighter(name, FIGHTER_SHIELD, FIGHTER_ATTACK, FIGHTER_LIFE);
-        }else if (type == 'c'){
+        } else if (type == 'c') {
             player2 = new Fighter(name, RANGER_SHIELD, RANGER_ATTACK, RANGER_LIFE);
-        }else{
+        } else {
             System.err.println("This should never happen!");
             System.exit(1);
         }
-        
-        System.out.println(player1);
-        System.out.println(player2);
 
-        
+        System.out.println("Selecting First Player!!");
+        if (Math.random() > 0.5) {
+            Fighter temp = player1;
+            player1 = player2;
+            player2 = temp;
+        }
+
+        System.out.println("Player 1: " + player1);
+        System.out.println("Player 2: " + player2);
+        System.out.println("\n\nGame Starts!!\n\n");
+        boolean keepOn;
+        while (true) {
+            keepOn = play(player1, player2);
+            if (!keepOn){
+                System.out.println("Player 1 Wins: " + player1);
+                break;
+            }
+            keepOn = play(player2, player1);
+            if (!keepOn){
+                System.out.println("Player 2 Wins: " + player2);
+                break;
+            }
+            
+        }
     }
-    private static char selectType(){
+
+    private static boolean play(Fighter player1, Fighter player2) {
+        int power, attack, defenseStatus;
+        System.out.println("Atacking Player: " + player1.getName());
+
+        do {
+            System.out.print("How much power you want to use: ");
+            power = input.nextInt();
+            if (power < 0 || power > 10) {
+                System.err.println("Incorrect Value, input again [1..10]");
+            }
+        } while (power < 0 || power > 10);
+        attack = player1.attack(power);
+        System.out.println("Current atack = " + attack);
+        System.out.println("Attacking Player Stats = " + player1);
+        System.out.println();
+
+        defenseStatus = player2.defend(attack);
+        System.out.println("Defending Player Defended!!");
+        System.out.println("Defending Player Stats = " + player2);
+        if (defenseStatus == Fighter.SHIELD_ON) {
+            System.out.println("Defending Player still has shield");
+        } else if (defenseStatus == Fighter.LIFE_ON) {
+            System.out.println("Defending Player has no shield, but has life");
+        } else {
+            System.out.println("Defending Player is dead");
+            return false;
+        }
+        return true;
+    }
+
+    private static char selectType() {
         System.out.println("Troop Type:");
         System.out.println("\ta. Paladin");
         System.out.println("\tb. Fighter");
         System.out.println("\tc. Ranger");
         System.out.print("Choice: ");
         String choice;
-        do{
+        do {
             choice = input.nextLine();
-            if (choice.length() != 1 || (choice.charAt(0) != 'a' && choice.charAt(0) != 'b' && choice.charAt(0) != 'c')){
+            if (choice.length() != 1 || (choice.charAt(0) != 'a' && choice.charAt(0) != 'b' && choice.charAt(0) != 'c')) {
                 System.err.println("Incorrect choice, please input again");
                 System.out.print("Choice: ");
-            }else
+            } else {
                 break;
-        }while (true);
+            }
+        } while (true);
         return choice.charAt(0);
     }
 }
